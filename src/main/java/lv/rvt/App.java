acc3f5fd1109b148693dc126339b33c3dcad1e05
ArtursBlumenfelds <167060040+ArtursBlumenfelds@users.noreply.github.com>
2024-09-13 11:26:33 +0000

    Uzdevums Nr.

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,9 +1,40 @@
 package lv.rvt;
 
+import java.util.*;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        // Make your Hello World programm!
+        System.out.println("Hello RVT!");
+        System.out.println("... and the universe!");
+        //sout = print
+        String name = "Artūrs";
+        int age = 20;
+        boolean isOnline = false;
+        
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(isOnline);
+
+        // Scanner  scanneer = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Jūsu vards: ");
+        System.out.println("___");
+        String name1 = scanner.nextLine(); //returns to ievadi
+
+        System.out.println("Jūsu Uzvārds: ");
+        System.out.println("___");
+        String surname = scanner.nextLine();
+
+        System.out.println("Jūsu grupa: ");
+        System.out.println("___");
+        String group = scanner.nextLine();
+
+        System.out.println("Students: " + name1 + " " + surname + ", grupa: " + group);
+
+
+
     }
 }
