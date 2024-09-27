5dabdd8e32827ee4ef4b34da4c6567dd7609240b
ArtursBlumenfelds <167060040+ArtursBlumenfelds@users.noreply.github.com>
2024-09-27 10:50:01 +0000

    Points/27.09.2024

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -23,16 +23,34 @@ public class App
 
         
         
-        System.out.println("Give the speed:");
-        int speed = Integer.valueOf(scanneer.nextLine());
-        if (speed > 120)
+        System.out.println("Give the points[0-100]:");
+        int points = Integer.valueOf(scanneer.nextLine());
+        if (points < 0)
         {
-            System.out.println("Speeding ticket!");
+            System.out.println("Impossible!");
         }
-        else {
-            System.out.println("Your good to go!");
+        if (points > 0 && points <= 49){
+            System.out.println("failed");
         }
-
+        if (points >= 50 && points <= 59){
+            System.out.println("1");
+        }
+        if (points >= 60 && points <= 69){
+            System.out.println("2");
+        }
+        if (points >= 70 && points <= 79){
+            System.out.println("3");
+        }
+        if (points >= 80 && points <= 89){
+            System.out.println("4");
+        }
+        if (points >= 90 && points <= 100){
+            System.out.println("5");
+        }
+        if (points > 100){
+            System.out.println("incredible!");
+        }
+        
         
 
 
