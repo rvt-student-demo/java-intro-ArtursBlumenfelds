1b31c21c580adfbf5eb683840434bdc89a9a0d0e
ArtursBlumenfelds <167060040+ArtursBlumenfelds@users.noreply.github.com>
2024-10-25 10:11:33 +0000

    Star sign

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -19,19 +19,40 @@ public class App
         //  double floatingPoint = Double.valueOf(scanner.nextLine());
         //  boolean trueOrFalse = Boolean.valueOf(scanner.nextLine());
         
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("How many times?");
-        int integer = Integer.valueOf(scanner.nextLine());
-        int time = 0;
-        while (time < integer){
-            print();
-            time = time + 1;
-        }
-       
-    }
-    public static void print()
-    {    
-        System.out.println("In a hole in the ground there lived a method"); 
-    }
+        printStars(5);
+        printStars(3);
+        printStars(9);
+        System.out.println("");
+        printSquare(4);
+        printRectangle(17, 3);
+        printTriangle(4);}
 
-}   
+        public static void printStars(int number) {
+            for(int i = 0; i < number; i++) {
+                  System.out.print("*");
+                  
+            }
+          System.out.println("");
+        }
+        public static void printSquare(int size) {
+            for(int i = 0; i < size; i++) {
+                  printStars(size);
+                  
+            }
+          System.out.println("");
+        }
+        public static void printRectangle(int width, int height) {
+            for(int i = 0; i < height; i++) {
+                  printStars(width);
+                  
+            }
+          System.out.println("");
+        }
+        public static void printTriangle(int size) {
+            for(int i = 0; i <= size; i++) {
+                  printStars(i);
+                  
+            }
+          System.out.println("");
+        }
+    }
