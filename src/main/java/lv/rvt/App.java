371cfb7d51116ecf55169e387e1cf6968a59561d
ArtursBlumenfelds <167060040+ArtursBlumenfelds@users.noreply.github.com>
2024-11-01 10:32:57 +0000

    Exercise.Nr2

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -19,14 +19,17 @@ public class App
         //  double floatingPoint = Double.valueOf(scanner.nextLine());
         //  boolean trueOrFalse = Boolean.valueOf(scanner.nextLine());
       
-        int sum = 0;
-        int[] val = {0, 1, 2, 3}; 
+        int[] val = {13, -4, 82, 17}; 
+        int[] twice = new int [4];
         
-        for(int i =0;i<=val.length-1;i++){
-            sum = sum + val[i];
+        System.out.println( "Original Array: " 
+            + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
+      
+        for(int i=0;i<=val.length-1;i++){
+            twice[i] = val[i] * 2;
         }
-        
-     
-        System.out.println( "Sum of all numbers = " + sum );
+        System.out.println( "New Array: " 
+            + twice[0] + " " + twice[1] + " " + twice[2] + " " + twice[3] );
+       
     }
 }
