bd0026122efee7d1f9a9002a3211449545cf8182
ArtursBlumenfelds <167060040+ArtursBlumenfelds@users.noreply.github.com>
2024-11-01 10:39:25 +0000

    Exercise.Nr4

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -19,17 +19,22 @@ public class App
         //  double floatingPoint = Double.valueOf(scanner.nextLine());
         //  boolean trueOrFalse = Boolean.valueOf(scanner.nextLine());
       
-        
-        int[] valA = { 13, -22,  82,  17}; 
-        int[] valB = {-12,  24, -79, -13};
-        int[] sum = {  0,   0,   0,   0};
+        int[] valA   = { 13, -22,  82,  17}; 
+        int[] valB   = {  0,   0,   0,   0};
 
-        for(int i=0;i<=3;i++){
-        sum[i] = valA[i] + valB[i];
-    }
-        System.out.println( "sum: " 
-            + sum[0] + " " + sum[1] + " " + sum[2] + " " + sum[3] );
-       
-        
+        valB[0] = 12;
+        valB[1] = 47;
+        valB[2] = -57;
+        valB[3] = 8;
+
+        System.out.println( "valA: " 
+            + valA[0] + " " + valA[1] + " " + valA[2] + " " + valA[3] );
+     
+        System.out.println( "valB: " 
+            + valB[0] + " " + valB[1] + " " + valB[2] + " " + valB[3] );
+    
+        System.out.println( "sum:  " 
+            + (valA[0]+valB[0]) + " " + (valA[1]+valB[1]) + " " 
+            + (valA[2]+valB[2]) + " " + (valA[3]+valB[3]) );      
     }
 }
