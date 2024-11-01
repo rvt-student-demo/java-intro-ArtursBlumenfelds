0efb753e3df90d5edc8bf89ca6310196de0686f3
ArtursBlumenfelds <167060040+ArtursBlumenfelds@users.noreply.github.com>
2024-11-01 10:38:29 +0000

    Exercise.Nr3

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -19,17 +19,17 @@ public class App
         //  double floatingPoint = Double.valueOf(scanner.nextLine());
         //  boolean trueOrFalse = Boolean.valueOf(scanner.nextLine());
       
-        int[] val = {13, -4, 82, 17}; 
-        int[] twice = new int [4];
         
-        System.out.println( "Original Array: " 
-            + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
-      
-        for(int i=0;i<=val.length-1;i++){
-            twice[i] = val[i] * 2;
-        }
-        System.out.println( "New Array: " 
-            + twice[0] + " " + twice[1] + " " + twice[2] + " " + twice[3] );
+        int[] valA = { 13, -22,  82,  17}; 
+        int[] valB = {-12,  24, -79, -13};
+        int[] sum = {  0,   0,   0,   0};
+
+        for(int i=0;i<=3;i++){
+        sum[i] = valA[i] + valB[i];
+    }
+        System.out.println( "sum: " 
+            + sum[0] + " " + sum[1] + " " + sum[2] + " " + sum[3] );
        
+        
     }
 }
