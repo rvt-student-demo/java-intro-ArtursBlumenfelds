d841d9012428bb28b6c0dd68e824c22ddd46526a
ArtursBlumenfelds <167060040+ArtursBlumenfelds@users.noreply.github.com>
2024-11-15 11:37:19 +0000

    Statistics

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,12 +1,9 @@
 package lv.rvt;
 
-import java.util.*;
+import java.util.Scanner;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        //  System.out.println("Hello RVT!");
+public class App {
+
         //  sout = print
         //  String name = "Artūrs";
         //  int age = 20;
@@ -18,40 +15,40 @@ public class App
         //  int integer = Integer.valueOf(scanner.nextLine());
         //  double floatingPoint = Double.valueOf(scanner.nextLine());
         //  boolean trueOrFalse = Boolean.valueOf(scanner.nextLine());
-      
+    
+    public static void main(String[] args) {
+        
         Scanner scanneer = new Scanner(System.in);
-        ArrayList<String> names = new ArrayList<>();
-        ArrayList<Integer> date = new ArrayList<>();
+        
+        Statistics statistics = new Statistics();
+        Statistics evens = new Statistics();
+        Statistics odds = new Statistics();
+
+        System.out.println("Enter numbers:");
+
         while (true) {
-            System.out.println("Enter a name and birth: ");
-            String nameBirth = scanneer.nextLine();
-            if (nameBirth.isEmpty()){
+            int num = scanneer.nextInt();
+
+            if (num == -1) {
                 break;
             }
-            String[] part = nameBirth.split(",");
-            String name = part[0];
-            int birthyear = Integer.parseInt(part[1]);
-        
-            names.add(name);
-            date.add(birthyear);
-        }
-        String longname = "";
-        for(String name : names){
-            if(name.length() > longname.length()){
-                longname = name;
-            }
-        }
-        int sum = 0;
-        for(int year : date){
-            sum += year;
-        }
-        
-        double avg = sum / (double) date.size();
-        System.out.println("Longest name: " + longname);
-        System.out.println("Average of the birth years: " + avg);
-    }
+            statistics.addNumber(num);
 
+            if (num % 2 == 0) {
+                evens.addNumber(num);
+            } else {
+                odds.addNumber(num);
+            }
+
+        }
+
+        System.out.println(statistics.average());
+        System.out.println("Sum: " + statistics.sum());
+        System.out.println("Sum of even numbers: " + evens.sum());
+        System.out.println("Sum of odd numbers: " + odds.sum());
+    }
 }
+
 
 
 
