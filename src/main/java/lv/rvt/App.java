d11ac5b800a08adcd9cc25c5c4bdc95ca1c01af7
ArtursBlumenfelds <167060040+ArtursBlumenfelds@users.noreply.github.com>
2025-02-14 12:37:54 +0000

    Interface In A Box, fixed

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -21,7 +21,17 @@ public class App {
                 CD cd1 = new CD("Pink Floyd", "Dark Side of the Moon", 1973);
                 CD cd2 = new CD("Wigwam", "Nuclear Nightclub", 1975);
                 CD cd3 = new CD("Rendezvous Park", "Closer to Being Here", 2012);
-            
+                
+                Box box = new Box(10);
+                
+		box.add(new Book("Fyodor Dostoevsky", "Crime and Punishment", 2)) ;
+		box.add(new Book("Robert Martin", "Clean Code", 1));
+		box.add(new Book("Kent Beck", "Test Driven Development", 0.7));
+
+		box.add(new CD("Pink Floyd", "Dark Side of the Moon", 1973));
+		box.add(new CD("Wigwam", "Nuclear Nightclub", 1975));
+		box.add(new CD("Rendezvous Park", "Closer to Being Here", 2012));
+
                 System.out.println(book1);
                 System.out.println(book2);
                 System.out.println(book3);
