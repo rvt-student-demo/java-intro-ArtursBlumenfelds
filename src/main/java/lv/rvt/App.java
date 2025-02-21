45bb7765f58d81d6cbf83fafb48d2ee14b9571c3
ArtursBlumenfelds <167060040+ArtursBlumenfelds@users.noreply.github.com>
2025-02-21 12:16:40 +0000

    21.02.2025

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,6 +1,10 @@
 package lv.rvt;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+
 public class App {
+        
         //  sout = print
         //  String name = "Artūrs";
         //  int age = 20;
@@ -14,31 +18,27 @@ public class App {
         //  boolean trueOrFalse = Boolean.valueOf(scanner.nextLine());
 
         public static void main (String[] args){
-                Book book1 = new Book("Fyodor Dostoevsky", "Crime and Punishment", 2);
-                Book book2 = new Book("Robert Martin", "Clean Code", 1);
-                Book book3 = new Book("Kent Beck", "Test Driven Development", 0.5);
-            
-                CD cd1 = new CD("Pink Floyd", "Dark Side of the Moon", 1973);
-                CD cd2 = new CD("Wigwam", "Nuclear Nightclub", 1975);
-                CD cd3 = new CD("Rendezvous Park", "Closer to Being Here", 2012);
-                
-                Box box = new Box(10);
-                
-		box.add(new Book("Fyodor Dostoevsky", "Crime and Punishment", 2)) ;
-		box.add(new Book("Robert Martin", "Clean Code", 1));
-		box.add(new Book("Kent Beck", "Test Driven Development", 0.7));
 
-		box.add(new CD("Pink Floyd", "Dark Side of the Moon", 1973));
-		box.add(new CD("Wigwam", "Nuclear Nightclub", 1975));
-		box.add(new CD("Rendezvous Park", "Closer to Being Here", 2012));
+        ArrayList<Integer> list = new ArrayList<Integer>();
 
-                System.out.println(book1);
-                System.out.println(book2);
-                System.out.println(book3);
-                System.out.println(cd1);
-                System.out.println(cd2);
-                System.out.println(cd3);
+        list.add(5);
+        list.add(3);
+        list.add(33);
+        list.add(199);
+        list.add(105);
 
+        list.sort(Comparator.naturalOrder());
+
+        list.sort(Comparator.reverseOrder());
+
+        System.out.println("\n");
+        System.out.println("=================");
+        System.out.println("| index | value |");
+        System.out.println("=================");
+        for(int i = 0; i < list.size(); i++){
+        System.out.printf("| %-5d | %-5d | \n", i, list.get(i));
+        }
+        System.out.println("=================");
 
 }       
 }       
